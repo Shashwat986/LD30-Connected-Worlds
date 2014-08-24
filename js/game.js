9b48f240a7cea41e4ea4044303fb02551ced7052
Shashwat986 <chandra.shashwat@gmail.com>
2014-08-24 15:11:51 +0530

    Hero movement
    Also health refreshing, and end_of_game template

--- js/game.js
+++ js/game.js
@@ -1,7 +1,51 @@
 map_heaven = {};
 map_hell = {};
 map_earth = {};
+me = {};
 game_status = "earth";
+game_list = [];
+power = { "heaven":50, "hell":50, "earth":100 };
+
+function key_pressed(key)
+{
+	switch(key)
+	{
+		case 37:
+			// Left
+			var ind = game_list.indexOf(me[game_status]);
+			eval('check1 = map_'+game_status+'[game_list[ind-1]];');
+			eval('check = map_'+game_status+'[game_list[ind]];');
+			if (!check1 && game_list[ind-1])
+			{
+				eval('map_'+game_status+'[game_list[ind]] = "";');
+				eval('map_'+game_status+'[game_list[ind-1]] = "me";');
+				me[game_status] = game_list[ind-1];
+				refresh();
+				return 0;
+			}
+			else
+				return 0;
+		case 39:
+			// Right
+			var ind = game_list.indexOf(me[game_status]);
+			eval('check1 = map_'+game_status+'[game_list[ind+1]];');
+			eval('check = map_'+game_status+'[game_list[ind]];');
+			if (!check1 && game_list[ind+1])
+			{
+				eval('map_'+game_status+'[game_list[ind]] = "";');
+				eval('map_'+game_status+'[game_list[ind+1]] = "me";');
+				me[game_status] = game_list[ind+1];
+				refresh();
+				return 0;
+			}
+			else
+				return 0;
+		default:
+			console.log(key);
+			return 0;
+	}
+}
+
 
 function load()
 {
@@ -29,6 +73,15 @@ function load()
 	map_earth["11"]="me";
 	map_heaven["11"]="me";
 	map_hell["11"]="me";
+
+	me["hell"] = "11";
+	me["earth"] = "11";
+	me["heaven"] = "11";
+
+	game_list = []
+	$(".game").each(function(){
+		game_list.push(this.id);
+	});
 
 	// Hell
 	block_list = []
@@ -72,11 +125,19 @@ function to_html(map_char)
 {
 	switch(true)
 	{
+		case(!map_char):
+			return "";
 		case(map_char == "me"):
 			return '<img src="images/f_'+game_status+'.png" class="sprite" alt="You"/>';
 		case(typeof map_char === "undefined"):
 			return "";
 		case(map_char.charAt(0) == "e"):
+			val = Math.floor(Math.random() * 7) + 1;
+			return '<img src="images/enemy'+val+'.png" class="sprite" alt="Power '+map_char.substring(1)+'"/><br/>'+map_char.substring(1);
+		case(map_char.charAt(0) == 'g'):
+			val = Math.floor(Math.random() * 7) + 1;
+			return '<img src="images/enemy'+val+'.png" class="sprite" alt="Power '+map_char.substring(1)+'"/><br/>'+map_char.substring(1);
+		case(map_char.charAt(0) == 'b'):
 			val = Math.floor(Math.random() * 7) + 1;
 			return '<img src="images/enemy'+val+'.png" class="sprite" alt="Power '+map_char.substring(1)+'"/><br/>'+map_char.substring(1);
 		default:
@@ -137,7 +198,7 @@ function hell()
 function refresh(clear)
 {
 	clear = typeof clear !== "undefined" ? a : 0;	// Chrome compatibility.
-	
+
 	$(".game").html("");
 	if (clear == 0)
 		$(".game").html(function(){
@@ -156,5 +217,28 @@ function refresh(clear)
 					break;
 			}
 		});
+
+	$("#heaven-health").css("width",power["heaven"]+"%");
+	$("#hell-health").css("width",power["hell"]+"%");
+	$("#earth-health").css("width",power["earth"]+"%");
+
+	var eg = end_game()
+	if(eg==1)
+	{
+		// Victory
+	}
+	else if (eg==-1)
+	{
+		// Loss
+	}
+	else
+	{
+		// Nothing
+
+	}
 }
 
+function end_game()
+{
+	return 0;
+}
